73db7846023e422dac3d2c86b51a434972cc7ca1
death667b <idaniel@spinnys.com>
2017-09-24 22:25:50 +1000

    Seems to work, but output still array

--- cavityMap.js
+++ cavityMap.js
@@ -12,10 +12,25 @@ const input1 = `4
 cavityMap(input1);
 function cavityMap(input) {
     let [n, ...theMap] = input.split('\n');
+    n = parseInt(n);
     theMap = theMap
         .map( row => row.split('')
         .map( singles => parseInt(singles)
     ));
+
+    let currentSquare;
+    for (let x = 1; x < n-1; x++) {
+        for (let y = 1; y < n-1; y++) {
+            currentSquare = theMap[x][y];
+            if (theMap[x-1][y] < currentSquare &&
+                theMap[x+1][y] < currentSquare &&
+                theMap[x][y-1] < currentSquare &&
+                theMap[x][y+1] < currentSquare) {
+                    theMap[x][y] = 'X';
+                    
+                }
+        }
+    }
 
     console.log(theMap)
 }
