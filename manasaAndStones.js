0bffd37bbd60a6d6e66f67c1bdc7815dc43a1da2
death667b <idaniel@spinnys.com>
2017-09-26 21:39:58 +1000

    First attempt at solution

--- manasaAndStones.js
+++ manasaAndStones.js
@@ -16,7 +16,37 @@ function processData(input) {
     
     for (let t = 0; t < T; t++) {
         [n, a, b, ...x] = x;
-        console.log(n,a,b,x);  
+         
+        let adder = b-a;
+        let currentNumber = --n*a;
+        let displayNumber = '' + currentNumber.toString();
 
+        for (let x = 0; x < n; x++) {
+            currentNumber += adder;
+            displayNumber += ' ' + currentNumber.toString();
+        }
+        
+        console.log(displayNumber);
     }
 } 
+/*
+
+  5 10 100
+b-a = 100-10 = 90
+10*--n
+
+
+
+  0, 10, 20, 30, 40         40 +
+  0, 10, 20, 30, 130        90
+  0, 10, 20, 120, 130       0    
+  0, 10, 110, 120, 130      0
+  0, 100, 110, 120, 130     0
+  0, 100, 110, 120, 220     90
+  0, 100, 110, 210, 220     0
+  0, 100, 200, 210, 220     0
+  0, 100, 200, 210, 310     90
+  0, 100, 200, 300, 310     0
+  0, 100, 200, 300, 400     90
+
+  */
