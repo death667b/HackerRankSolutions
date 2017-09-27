9442352a9e129ec0a9939a7e715ea5d345ecf849
Ian Daniel <idaniel@spinnys.com>
2017-09-27 12:49:53 +1000

    Working solution day13

--- day13-abstractClasses.js
+++ day13-abstractClasses.js
@@ -7,16 +7,6 @@ const input1 = `The Alchemist
 Paulo Coelho
 248`;
 
-
-mainProgram(input1);
-function mainProgram(input) {
-    const [title, authoer, price] = input.split('\n');
-
-    let book = new MyBook(title, author, price)
-    book.display()
-}
-
-
 class Book {
     constructor(title, author) {
         if (this.constructor === Book) {
@@ -33,21 +23,24 @@ class Book {
     }
 }
 
-// Declare your class here.
-    /**   
-    *   Class Constructor
-    *   
-    *   @param title The book's title.
-    *   @param author The book's author.
-    *   @param price The book's price.
-    **/
-    // Write your constructor here
-    
-    /**   
-    *   Method Name: display
-    *   
-    *   Print the title, author, and price in the specified format.
-    **/
-    // Write your method here
-    
-// End class
+class MyBook extends Book {
+    constructor(title, author, price) {
+        super(title, author);
+
+        this.price = price;
+    }
+
+    display() {
+        console.log('Title:', this.title);
+        console.log('Author:', this.author);
+        console.log('Price:', this.price);
+    }
+}
+
+mainProgram(input1);
+function mainProgram(input) {
+    const [title, author, price] = input.split('\n');
+
+    let book = new MyBook(title, author, price)
+    book.display()
+}
