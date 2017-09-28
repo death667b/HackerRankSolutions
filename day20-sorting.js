436fbf594251556d3fd39681a58908af758ce9cf
death667b <idaniel@spinnys.com>
2017-09-28 17:47:24 +1000

    working code

--- day20-sorting.js
+++ day20-sorting.js
@@ -10,10 +10,29 @@ const input2 = `3
 // First Element: 1
 // Last Element: 3
 
-bubbleSort(input2);
+bubbleSort(input1);
 function bubbleSort(input) {
     let [n, a] = input.split('\n');
+    let totalSwaps = 0;
     a = a.split(' ').map( x => parseInt(x));
 
-    console.log(a)
+    for (let x = 0; x < n; x++) {
+        let numberOfSwaps = 0;
+
+        for (let j = 0; j < n-1; j++) {
+            let placeHolder;
+            if (a[j] > a[j+1]) {
+                placeHolder = a[j];
+                a[j] = a[j+1];
+                a[j+1] = placeHolder;
+                numberOfSwaps++;
+                totalSwaps++;
+            }
+        }
+
+        if (numberOfSwaps == 0) break;
+    }
+    console.log(`Array is sorted in ${totalSwaps} swaps.`);
+    console.log(`First Element: ${a[0]}`);
+    console.log(`Last Element: ${a[n-1]}`);
 }
